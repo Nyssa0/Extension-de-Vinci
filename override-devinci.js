546c9ac152b950549256e9c568b4a55dced306f5
Mehdi <mehdialsidcheikh@gmail.com>
2021-09-17 13:04:17 +0200

    :lipstick: change style buttons

--- override-devinci.js
+++ override-devinci.js
@@ -1,1 +1,7 @@
-document.getElementsByTagName('navbar-inner')[0].classList.add('override')
+document.getElementsByTagName('body')[0].classList.add('override');
+
+var menuButtons = document.querySelectorAll('.tools a')
+for(let i = 0; i < menuButtons.length; i++) {
+    let hey = 'button-custom'+i
+    menuButtons[i].classList.add(hey);
+}
